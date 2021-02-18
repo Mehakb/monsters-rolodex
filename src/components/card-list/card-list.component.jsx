0184c3ceee4d6267f6f85bb0eccca30e14034586
mehak <mehak1921@yahoo.com>
2021-02-18 22:34:17 +0530

    Add key for cards.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,7 +3,7 @@ import "./card-list.styles.css";
 export const CardList = (props) => (
     <div className="card-list">
         {props.monsters.map((monster) =>
-            <Card monster={monster} />
+            <Card key={monster.id} monster={monster} />
         )}
     </div>
 )
